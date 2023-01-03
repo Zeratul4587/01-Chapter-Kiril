6088ab7b848c38e21baf6ad95215678537354fb4
Zeratul4587 <margaritovkl@gmail.com>
2023-01-03 16:54:34 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_Chapter_Kiril
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
